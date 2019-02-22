b1eb52a8437d102cb16fd3a8d22a6cf8f54d5b70
Rudy Ravelin <rudy@noukod.com>
2019-02-22 19:09:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = "Rudy Ravelin";
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
