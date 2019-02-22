ae7b1120f2cd8ec02cdfffb1356071e5e7cdb1f6
Rudy Ravelin <rudy@noukod.com>
2019-02-22 18:54:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 // Write your code in this file!
 const currentUser = "Rudy Ravelin";
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
